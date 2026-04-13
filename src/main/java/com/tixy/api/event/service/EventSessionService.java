dab39da786e042969338b912cdd50066dbe803f4
jasmin <cjm98@smartel.co.kr>
2026-04-14 00:26:34 +0900

    add : session count 추가

--- src/main/java/com/tixy/api/event/service/EventSessionService.java
+++ src/main/java/com/tixy/api/event/service/EventSessionService.java
@@ -44,7 +44,7 @@ public class EventSessionService {
         EventSession eventSession = EventSession.builder()
                 .event(event)
                 .session(sessions.session())
-                .sessionSeatCount(100L)
+                .sessionSeatCount(sessions.sessionSeatCount())
                 .status(EventSessionStatus.SCHEDULED)
                 .sessionSeatCount(seatCount)
                 .sessionOpenDate(sessions.sessionOpenDate())
