1c2fb5b44c61f389a95d8ab645157124fd3f1c9f
jasmin <cjm98@smartel.co.kr>
2026-04-14 00:24:40 +0900

    fix : 엔티티 컬럼 추가 되면서 정상적으로 동작하지 않는 코드 수정

--- src/main/java/com/tixy/api/event/service/EventSessionService.java
+++ src/main/java/com/tixy/api/event/service/EventSessionService.java
@@ -44,6 +44,7 @@ public class EventSessionService {
         EventSession eventSession = EventSession.builder()
                 .event(event)
                 .session(sessions.session())
+                .sessionSeatCount(100L)
                 .status(EventSessionStatus.SCHEDULED)
                 .sessionSeatCount(seatCount)
                 .sessionOpenDate(sessions.sessionOpenDate())
