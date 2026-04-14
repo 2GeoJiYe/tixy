d6f6a53198f2f786b47d0b8b28dd5bedfb967f73
thetana <zxsa135@naver.com>
2026-04-14 18:17:06 +0900

    feat: 싯트 테스트 코드 추가

--- src/main/java/com/tixy/api/event/repository/EventSessionRepository.java
+++ src/main/java/com/tixy/api/event/repository/EventSessionRepository.java
@@ -1,10 +1,12 @@
 package com.tixy.api.event.repository;
 
 import com.tixy.api.event.entity.EventSession;
+import com.tixy.api.event.enums.EventSessionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface EventSessionRepository extends JpaRepository<EventSession,Long> {
     List<EventSession> findALlByEventId(Long eventId);
+    List<EventSession> findAllByStatus(EventSessionStatus status);
 }
